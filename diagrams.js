9ed821f7955bd97ad5dd905f9f55c96aeecc19cb
Christian Paul <info@chrpaul.de>
2016-02-06 23:32:23 -0800

    Set up the libraries and the main template.

---
+++ diagrams.js
@@ -0,0 +1,29 @@
+// Create and populate a data table.
+var data = new vis.DataSet();
+// create some nice looking data with sin/cos
+var counter = 0;
+var steps = 50;  // number of datapoints will be steps*steps
+var axisMax = 314;
+var axisStep = axisMax / steps;
+for (var x = 0; x < axisMax; x+=axisStep) {
+    for (var y = 0; y < axisMax; y+=axisStep) {
+        var value = (Math.sin(x/50) * Math.cos(y/50) * 50 + 50);
+        data.add({id:counter++,x:x,y:y,z:value,style:value});
+    }
+}
+
+// specify options
+var options = {
+    width:  '100%',
+    height: 'auto',
+    style: 'surface',
+    showPerspective: true,
+    showGrid: true,
+    showShadow: false,
+    keepAspectRatio: true,
+    verticalRatio: 0.5
+};
+
+// Instantiate our graph object.
+var container = document.getElementById('visualization');
+var graph3d = new vis.Graph3d(container, data, options);
