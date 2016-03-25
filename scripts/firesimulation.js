f8f0abcd68c8abc9cb4e51669fe144c515479af5
Christian Paul <info@jaller.de>
2016-03-24 17:43:05 -0700

    Added checkbox to toggle the Heightmap

--- scripts/firesimulation.js
+++ scripts/firesimulation.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 class FireSimulation {
 	constructor( func, canvas, amount ) {
@@ -7,6 +7,7 @@ class FireSimulation {
 		console.log(canvas);
 		this.display = new FireDisplay( this.canvas );
 		this.heightmap = this.display.generateHeightMap( this.func, 0, 2 );
+		this.layerheightmap = false;
 		
 		this.reset();
 	}
@@ -20,24 +21,37 @@ class FireSimulation {
 		this.fireflies = levy2D( this.func, this.fireflies );
 		this.draw();
 	}
-	
+
 	draw( ) {
 		this.display.clear( );
-		this.display.drawImage( this.heightmap );
+		if (this.layerheightmap) {
+			this.display.drawImage( this.heightmap );
+		}
 		this.display.drawGrid( );
 		this.display.drawFireflies( this.fireflies );
 	}
-	
-	static insertIntoHTML( root ) {
-		
 
+	toogleHeightmap( ) {
+		console.log('toogleHeightmap');
+		if (this.checkbox_heightmap) {
+			this.layerheightmap = this.checkbox_heightmap.checked;
+		} else {
+			this.layerheightmap = !this.layerheightmap;
+		}
+		this.draw();
+	}
+
+	static insertIntoHTML( root ) {
 		var wrapper = document.createElement('div');
 		$(wrapper).addClass('col-xs-12 col-sm-6 col-sm-4');
 		var wrapper_canvas = document.createElement('div');
-		$(wrapper_canvas).addClass('embed-responsive embed-responsive-4by3')
+		$(wrapper_canvas).addClass('canvas-wrapper')
 			.appendTo( $(wrapper) );
-		var wrapper_button = document.createElement('div');
-		$(wrapper_button).addClass('btn-group form-group')
+		var wrapper_buttons = document.createElement('div')
+		$(wrapper_buttons).addClass('btn-group form-group')
+			.appendTo( $(wrapper) );
+		var wrapper_layers = document.createElement('div')
+		$(wrapper_layers).addClass('form-group')
 			.appendTo( $(wrapper) );
 
 		var canvas_id = 'canvas' + $('canvas').length;
@@ -48,23 +62,37 @@ class FireSimulation {
 			width: '500',
 			height: '500'
 		});
+		canvas.innerHTML = 'Sorry, your browser doesn\'t support the &lt;canvas&gt; element.';
 
 		canvas.appendTo( $(wrapper_canvas) );
 
 		var button_reset = $('<button type="button">Reset</button>')
 			.addClass('btn btn-default')
-			.appendTo( $(wrapper_button) );
+			.appendTo( $(wrapper_buttons) );
 
 		var button_next = $('<button type="button">Next</button>')
 			.addClass('btn btn-default')
-			.appendTo( $(wrapper_button) );
-		
+			.appendTo( $(wrapper_buttons) );
+
+		var label_heightmap = $('<label>Heightmap</label>')
+			.addClass('checkbox-inline')
+			.appendTo( $(wrapper_layers) );
+
+		var checkbox_heightmap = $('<input type="checkbox">')
+			.addClass('form-control')
+			.appendTo( $(label_heightmap) );
+
 		$(wrapper).appendTo( $(root) );
 
 		var sim = new FireSimulation( func, canvas[0], 10 );
 		canvas.simulation = sim;
 
-		button_reset[0].addEventListener("click", sim.reset.bind(sim));
-		button_next[0].addEventListener("click", sim.act.bind(sim));
+		// Buttons for Playback
+		button_reset[0].addEventListener('click', sim.reset.bind(sim));
+		button_next[0].addEventListener('click', sim.act.bind(sim));
+
+		// Checkboxes for layers
+		sim.checkbox_heightmap = checkbox_heightmap[0];
+		sim.checkbox_heightmap.addEventListener('change', sim.toogleHeightmap.bind(sim));
 	}
 }
